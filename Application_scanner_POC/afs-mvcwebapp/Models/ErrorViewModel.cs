abf36b382adc41d4862f88129c3d238abd2b673e
Kai Chao <kchao@bchousing.org>
2023-10-23 16:52:57 -0700

    Initial setup for MVC web app and azure function app

---
+++ Application_scanner_POC/afs-mvcwebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace afs_mvcwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
