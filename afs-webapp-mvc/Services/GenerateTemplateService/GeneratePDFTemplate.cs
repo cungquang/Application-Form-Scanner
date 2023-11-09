730b9401baeeb84ab03a0e34b5bbeab7e77faae2
Hong Cung <hcung@bchousing.org>
2023-11-09 01:03:25 -0800

    Add GenerateTemplateService

---
+++ afs-webapp-mvc/Services/GenerateTemplateService/GeneratePDFTemplate.cs
@@ -0,0 +1,11 @@
+﻿namespace afs_webapp_mvc.Services.GenerateTemplateService
+{
+    public class GeneratePDFTemplate : IGenerateTemplateService
+    {
+        public async Task<string> GenerateTemplateAsync(string templateContent)
+        {
+            await Task.Delay(1000);
+            return "";
+        }
+    }
+}
---
+++ afs-webapp-mvc/Services/GenerateTemplateService/IGenerateTemplateService.cs
@@ -0,0 +1,16 @@
+﻿namespace afs_webapp_mvc.Services.GenerateTemplateService
+{
+    public interface IGenerateTemplateService
+    {
+        /*
+         * GenerateTemplateAsync
+         * Description: 
+         *  -> Generate a template per request (Ex: web view, pdf, excel...), and return to user with input data
+         * Input:
+         *  -> string templateContent: content will be loaded into template
+         * Output:
+         *  -> return a string - template with content
+         */
+        Task<string> GenerateTemplateAsync(string templateContent);
+    }
+}
