39144c4cefed3cb724f912034cd16eea094de0cc
Hong Cung <hcung@bchousing.org>
2023-11-09 23:29:30 -0800

    Change Azure Function name <afs-ocr-serverhandler.cs> to <AfsServerHandler.cs>

--- afs-ocr-serverhandler/AfsServerHandler.cs
+++ afs-ocr-serverhandler/AfsServerHandler.cs
@@ -10,11 +10,11 @@ using Newtonsoft.Json;
 
 namespace afs_ocr_serverhandler
 {
-    public static class Function1
+    public static class AfsServerHandler
     {
-        [FunctionName("afs-ocr-serverhandler")]
+        [FunctionName("AfsServerHandler")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
