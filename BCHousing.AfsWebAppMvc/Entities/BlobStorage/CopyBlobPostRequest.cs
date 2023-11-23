70af199b76ce010cc4e6b3ca8ff8801f7e08c744
Hong Cung <hcung@bchousing.org>
2023-11-23 09:33:57 -0800

    Update service and api from BlobStorage and

---
+++ BCHousing.AfsWebAppMvc/Entities/BlobStorage/CopyBlobPostRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BCHousing.AfsWebAppMvc.Entities.BlobStorage
+{
+    public class CopyBlobPostRequest
+    {
+        public string DestinationContainer {  get; set; }
+        public string? DestinationFolder { get; set; }   
+    }
+}
---
+++ BCHousing.AfsWebAppMvc/Entities/BlobStorage/GetBlobContentGetRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BCHousing.AfsWebAppMvc.Entities.BlobStorage
+{
+    public class GetBlobContentGetRequest
+    {
+        public string containerName {  get; set; }
+        public string fileName { get; set; }
+    }
+}
