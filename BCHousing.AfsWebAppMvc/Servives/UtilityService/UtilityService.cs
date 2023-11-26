23c57d541da0dabc1d51dfe2fca76eed30b4eb3b
Hong Cung <hcung@bchousing.org>
2023-11-26 01:11:00 -0800

    Modify UtlityService - make it static and add new functions

--- BCHousing.AfsWebAppMvc/Servives/UtilityService/UtilityService.cs
+++ BCHousing.AfsWebAppMvc/Servives/UtilityService/UtilityService.cs
@@ -1,15 +1,35 @@
-﻿using System.ComponentModel;
+﻿using BCHousing.AfsWebAppMvc.Models;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Text.Json;
 
 namespace BCHousing.AfsWebAppMvc.Servives.UtilityService
 {
-    public class UtilityService
+    public static class UtilityService
     {
-        public UtilityService() { }
-
-        public static Guid GenerateCustomGuid()
+        public static Guid GenerateSystemGuid()
         {
             return Guid.NewGuid();
+        }
+
+        public static Guid GenerateBCHousingGuid(UploadFileViewModel model)
+        {
+            return Guid.NewGuid();
+        }
+
+        public static async Task<string> SerializeMetadataAsync(UploadFileViewModel model)
+        {
+            Dictionary<string, string> metadata = new()
+            {
+                ["SubmitBy"] = $"{model.FirstName} {model.LastName}",
+                ["UserDeclaredType"] = $"{model.DocumentType}",
+                ["DocumentName"] = $"{model.UploadFile.FileName}"
+            };
+
+            // Retun metadata in form of JSon string
+            using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync<Dictionary<string, string>>(stream, metadata);
+            return System.Text.Encoding.UTF8.GetString(stream.ToArray());
         }
     }
 }
