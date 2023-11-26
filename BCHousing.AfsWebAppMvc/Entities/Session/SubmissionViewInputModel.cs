50456b611c41c951b154a45fdc006cfaa67a996e
Hong Cung <hcung@bchousing.org>
2023-11-26 02:19:21 -0800

    Modify SessionManagmentService and SubmissionViewInputModel - to create upload record

--- BCHousing.AfsWebAppMvc/Entities/Session/SubmissionViewInputModel.cs
+++ BCHousing.AfsWebAppMvc/Entities/Session/SubmissionViewInputModel.cs
@@ -4,14 +4,26 @@ namespace BCHousing.AfsWebAppMvc.Entities.Session
 {
     public class SubmissionViewInputModel
     {
-        public string? LastName { get; set; }
+        public SubmissionViewInputModel()
+        {
+            LastName = new();
+            FirstName = new();
+            DocumentType = new();
+            UploadFileName = new();
+            UploadFileSize = new();
+            IsSuccess = new();
+        }
 
-        public string? FirstName { get; set; }
+        public List<string>? LastName { get; set; }
 
-        public string? DocumentType { get; set; }
+        public List<string>? FirstName { get; set; }
 
-        public string? UploadFileName { get; set; }
+        public List<string>? DocumentType { get; set; }
 
-        public long UploadFileSize { get; set; }
+        public List<string>? UploadFileName { get; set; }
+
+        public List<long>? UploadFileSize { get; set; }
+
+        public List<bool>? IsSuccess { get; set; }
     }
 }
--- BCHousing.AfsWebAppMvc/Servives/SessionManagementService/SessionManagementService.cs
+++ BCHousing.AfsWebAppMvc/Servives/SessionManagementService/SessionManagementService.cs
@@ -13,20 +13,19 @@ namespace BCHousing.AfsWebAppMvc.Servives.SessionManagementService
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public void SetUploadViewData(UploadFileViewModel model)
+        public void SetSubmissionViewInputModel(UploadFileViewModel model, bool IsUploadSuccess)
         {
-
-            _httpContextAccessor.HttpContext.Session.Set(SessionKey.UploadFileInfoKey, new SubmissionViewInputModel
-            {
-                DocumentType = model.DocumentType,
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                UploadFileName = model.UploadFile.FileName,
-                UploadFileSize = model.UploadFile.Length
-            });
+            var newSubmission = GetSubmissionViewInputModel();
+            newSubmission.FirstName.Add(model.FirstName);
+            newSubmission.LastName.Add(model.LastName);
+            newSubmission.DocumentType.Add(model.DocumentType);
+            newSubmission.UploadFileName.Add(model.UploadFile.FileName);
+            newSubmission.UploadFileSize.Add(model.UploadFile.Length);
+            newSubmission.IsSuccess.Add(IsUploadSuccess);
+            _httpContextAccessor.HttpContext.Session.Set(SessionKey.UploadFileInfoKey, newSubmission);
         }
 
-        public SubmissionViewInputModel GetUploadFileViewModel() 
+        public SubmissionViewInputModel GetSubmissionViewInputModel() 
         {
             return _httpContextAccessor.HttpContext.Session.Get<SubmissionViewInputModel>(SessionKey.UploadFileInfoKey) ?? new SubmissionViewInputModel();
         }
