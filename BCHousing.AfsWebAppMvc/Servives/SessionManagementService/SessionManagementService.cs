c644e94e786efc45da6cfb4bf27aa124414162dd
Hong Cung <hcung@bchousing.org>
2023-11-25 23:49:10 -0800

    Add SessionManagement to manage session

---
+++ BCHousing.AfsWebAppMvc/Servives/SessionManagementService/SessionManagementService.cs
@@ -0,0 +1,39 @@
+﻿using BCHousing.AfsWebAppMvc.Entities.Session;
+using BCHousing.AfsWebAppMvc.Models;
+using Microsoft.Extensions.Azure;
+
+namespace BCHousing.AfsWebAppMvc.Servives.SessionManagementService
+{
+    public class SessionManagementService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public SessionManagementService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void SetUploadViewData(UploadFileViewModel model)
+        {
+
+            _httpContextAccessor.HttpContext.Session.Set(SessionKey.UploadFileInfoKey, new SubmissionViewInputModel
+            {
+                DocumentType = model.DocumentType,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UploadFileName = model.UploadFile.FileName,
+                UploadFileSize = model.UploadFile.Length
+            });
+        }
+
+        public SubmissionViewInputModel GetUploadFileViewModel() 
+        {
+            return _httpContextAccessor.HttpContext.Session.Get<SubmissionViewInputModel>(SessionKey.UploadFileInfoKey) ?? new SubmissionViewInputModel();
+        }
+
+        public void ClearSsession()
+        {
+            _httpContextAccessor.HttpContext.Session.Clear();
+        }
+    }
+}
---
+++ BCHousing.AfsWebAppMvc/Servives/UtilityService/UtilityService.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace BCHousing.AfsWebAppMvc.Servives.UtilityService
+{
+    public class UtilityService
+    {
+        public UtilityService() { }
+
+        public static Guid GenerateCustomGuid()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
