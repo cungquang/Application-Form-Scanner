75993124ab8c8e5916b0973df8abb44db3be0b9f
Hong Cung <hcung@bchousing.org>
2023-11-26 01:11:29 -0800

    Modify UploadFileViewModel and Create SubmissionViewInputModel

---
+++ BCHousing.AfsWebAppMvc/Entities/Session/SubmissionViewInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BCHousing.AfsWebAppMvc.Entities.Session
+{
+    public class SubmissionViewInputModel
+    {
+        public string? LastName { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? DocumentType { get; set; }
+
+        public string? UploadFileName { get; set; }
+
+        public long UploadFileSize { get; set; }
+    }
+}
--- BCHousing.AfsWebAppMvc/Models/UploadFileViewModel.cs
+++ BCHousing.AfsWebAppMvc/Models/UploadFileViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace BCHousing.AfsWebAppMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BCHousing.AfsWebAppMvc.Models
 {
     public class UploadFileViewModel
     {
+        [Required(ErrorMessage = "Please provide your last name")]
         public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Please provide your first name")]
         public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please select a valid option")]
+        [RegularExpression("^(Other|Safer|Rap)$", ErrorMessage = "Invalid selection type")]
         public string? DocumentType { get; set; }
+
+        [Required(ErrorMessage = "Please select a file to submit")]
         public IFormFile UploadFile { get; set; }   
     }
 }
