e89ef7950119b8fdd553524a0f0b8f1605c982b2
Kai Chao <kchao@bchousing.org>
2023-11-29 12:58:10 -0800

    Adding one service layer for Database access

--- BCHousing.AfsWebAppMvc/Servives/AfsDatabaseService/IAfsDatabaseService.cs
+++ BCHousing.AfsWebAppMvc/Servives/AfsDatabaseService/IAfsDatabaseService.cs
@@ -1,9 +1,22 @@
 ﻿using BCHousing.AfsWebAppMvc.Entities;
+using BCHousing.AfsWebAppMvc.Entities.Database;
 
 namespace BCHousing.AfsWebAppMvc.Servives.AfsDatabaseService
 {
     public interface IAfsDatabaseService
     {
         public Task<IList<SubmissionLog>?> GetAllSubmissionLogs();
+
+        public Task<IList<SubmissionLog>> GetAllSubmissionLogsSync();
+
+        public Task<SubmissionLog> GetSubmissionLogByUrl(string fileUrl);
+
+        public Task<string> CreateSubmissionLog(SubmissionLogRequest requestBody);
+
+        public Task<FormRecordRequest> CreateFormRecord(FormRecordRequest requestBody);
+
+        public Task<string> UpdatePathToFile(UpdateFilePath requestBody);
+
+        public Task<string> UpdateSubmissionLogAfterOCRExtraction(UpdateLogAfterOCRExtraction requestBody);
     }
 }
