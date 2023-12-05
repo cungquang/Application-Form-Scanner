a504f9d757af310fccba4f434d61e5ce520c8016
Kai Chao <kchao@bchousing.org>
2023-12-04 16:12:30 -0800

    Fix guid and keys type value in the model are not allow to be NULL

--- BCHousing.AfsWebAppMvc/Entities/Database/SubmissionLogRequest.cs
+++ BCHousing.AfsWebAppMvc/Entities/Database/SubmissionLogRequest.cs
@@ -2,17 +2,17 @@
 {
     public class SubmissionLogRequest
     {
-        public string? ClassifyType { get; set; }
-        public string? FileUrl { get; set; }
+        public string ClassifyType { get; set; }
+        public string FileUrl { get; set; }
     }
 
     public class UpdateFilePath { 
-        public string? CurrentFilePath { get; set; }
-        public string? NewFilePath { get; set;}
+        public string CurrentFilePath { get; set; }
+        public string NewFilePath { get; set;}
     }
 
     public class UpdateLogAfterOCRExtraction { 
-        public string? FileUrl { get; set; }
+        public string FileUrl { get; set; }
         public bool? isRead {  get; set; }
         public decimal? AvgConfidenceScore { get; set; }
         public string? PathToAnalysisReport { get; set; }
